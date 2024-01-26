44abb9e67b821ce40c1bc2e79881616787245ff6
baroooo <baranseltekin@gmail.com>
2024-01-26 11:56:53 +0100

    feat: remove forge use core types

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import { NetworkConfig, networks, NetworkName } from './config';
 import '@nomicfoundation/hardhat-toolbox';
-import '@nomicfoundation/hardhat-foundry';
 
 function getNetworkConfig(): NetworkConfig {
   const network = process.env.NETWORK;
@@ -28,36 +27,6 @@ const config: HardhatUserConfig = {
       chainId: getNetworkConfig().chainId,
       hardfork: 'berlin',
     },
-  },
-  paths: {
-    sources: './lib/mento-core/contracts',
-  },
-  typechain: {
-    outDir: 'typechain-types',
-    target: 'ethers-v6',
-  },
-  solidity: {
-    compilers: [
-      {
-        version: '0.5.0',
-      },
-      {
-        version: '0.5.13',
-      },
-      {
-        version: '0.5.17',
-      },
-      {
-        version: '0.8.18',
-        settings: {
-          viaIR: true,
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    ],
   },
 };
 
---
+++ test/MentoToken.test.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+import hre, { ethers } from 'hardhat';
+import {
+  getContractsByChainId,
+  ContractAddresses,
+} from '@mento-protocol/mento-sdk';
+import { MentoToken, MentoToken__factory } from '@mento-protocol/mento-core-ts';
+
+describe('Mento Token', function () {
+  const { provider } = ethers;
+  let governanceAddresses: ContractAddresses;
+  let mentoToken: MentoToken;
+
+  before(async function () {
+    const chainId = hre.network.config.chainId;
+
+    if (!chainId) {
+      throw new Error('Chain ID not found');
+    }
+
+    governanceAddresses = getContractsByChainId(chainId);
+    if (!governanceAddresses) {
+      throw new Error('Governance addresses not found for this chain');
+    }
+
+    mentoToken = MentoToken__factory.connect(
+      governanceAddresses.MentoToken,
+      provider as any,
+    );
+
+    console.log('\r\n========================');
+    console.log('Running Mento Token tests on network with chain id:', chainId);
+    console.log('========================\r\n');
+  });
+
+  it('Should have supply gte initial supply', async function () {
+    const totalSupply = await mentoToken.totalSupply();
+    expect(totalSupply).greaterThanOrEqual(350_000_000);
+  });
+});
